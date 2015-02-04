b20614fefdfbec51a646b5909988543a846bbf06
Patrick Byrne <code@patrickbyrne.net>
2015-02-03 20:32:37 -0600

    Also copy the images into the temp directory

--- commands/build.go
+++ commands/build.go
@@ -5,23 +5,30 @@ import (
 	"github.com/pbyrne/bucket/models"
 	"github.com/pbyrne/bucket/util"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"os"
+	"path"
 )
 
 func Build(bucket models.Bucket) {
 	dir, err := ioutil.TempDir("", "bucket")
 	util.PanicIf(err)
-	indexPath := dir + "/index.html"
+
+	indexPath := path.Join(dir, "/index.html")
 	index, err := os.Create(indexPath)
 	util.PanicIf(err)
 	defer os.RemoveAll(dir)
 	template, err := template.ParseFiles("templates/index.html")
-
 	template.Execute(index, bucket)
 
-	// debugging
-	data, err := ioutil.ReadFile(indexPath)
-	util.PanicIf(err)
-	fmt.Println(string(data))
+	for _, image := range bucket.Images() {
+		dest, err := os.Create(path.Join(dir, image.BaseName()))
+		util.PanicIf(err)
+		src, err := os.Open(image.Path)
+		util.PanicIf(err)
+		io.Copy(dest, src)
+	}
+
+	fmt.Println(dir)
 }

7965755c3d9c5a8685477f47c33a59c586e9884b
Patrick Byrne <code@patrickbyrne.net>
2015-02-15 14:17:06 -0600

    Extract BucketBuilder into its own file

--- commands/build.go
+++ commands/build.go
@@ -1,125 +1,14 @@
 package commands
 
 import (
-	"crypto/md5"
 	"fmt"
 	"github.com/pbyrne/bucket/models"
-	"github.com/pbyrne/bucket/util"
-	"html/template"
-	"io"
-	"io/ioutil"
-	"os"
-	"path"
-	"path/filepath"
-	"strings"
 )
 
 func Build(bucket models.Bucket) {
-	builder := NewBucketBuilder(bucket)
+	builder := models.NewBucketBuilder(bucket)
+	defer builder.CleanUp()
 
 	builder.Perform()
 	fmt.Println(builder)
-}
-
-type BucketBuilder struct {
-	bucket models.Bucket
-	dir    string
-}
-
-func NewBucketBuilder(bucket models.Bucket) BucketBuilder {
-	dir, err := ioutil.TempDir("", "bucket")
-	util.PanicIf(err)
-	return BucketBuilder{bucket: bucket, dir: dir}
-}
-
-func (bb BucketBuilder) Perform() {
-	defer bb.cleanUp()
-
-	bb.writeIndex()
-	bb.copyImages()
-	bb.buildAssets()
-}
-
-func (bb BucketBuilder) Images() []models.Image {
-	return bb.bucket.Images()
-}
-
-func (bb BucketBuilder) FingerprintedJavaScripts() []string {
-	result := make([]string, 0)
-	for _, jsSrc := range bb.javaScriptPaths() {
-		result = append(result, bb.fingerprintedBaseName(jsSrc))
-	}
-	return result
-}
-
-func (bb BucketBuilder) FingerprintedStylesheets() []string {
-	result := make([]string, 0)
-	for _, cssSrc := range bb.stylesheetPaths() {
-		result = append(result, bb.fingerprintedBaseName(cssSrc))
-	}
-	return result
-}
-
-func (bb BucketBuilder) writeIndex() {
-	index, err := os.Create(path.Join(bb.dir, "/index.html"))
-	util.PanicIf(err)
-	template, err := template.ParseFiles("templates/index.html")
-	util.PanicIf(err)
-	template.Execute(index, bb)
-}
-
-func (bb BucketBuilder) copyImages() {
-	for _, image := range bb.bucket.Images() {
-		bb.copyFile(path.Join(bb.dir, image.BaseName()), image.Path)
-	}
-}
-
-func (bb BucketBuilder) buildAssets() {
-	jsDestPath := path.Join(bb.dir, "javascripts")
-	err := os.Mkdir(jsDestPath, 0744)
-	util.PanicIf(err)
-	for _, jsSrc := range bb.javaScriptPaths() {
-		dest := path.Join(jsDestPath, bb.fingerprintedBaseName(jsSrc))
-		bb.copyFile(dest, jsSrc)
-	}
-
-	cssDestPath := path.Join(bb.dir, "stylesheets")
-	err = os.Mkdir(cssDestPath, 0744)
-	util.PanicIf(err)
-	for _, cssSrc := range bb.stylesheetPaths() {
-		dest := path.Join(cssDestPath, bb.fingerprintedBaseName(cssSrc))
-		bb.copyFile(dest, cssSrc)
-	}
-}
-
-func (bb BucketBuilder) copyFile(destPath string, srcPath string) {
-	dest, err := os.Create(destPath)
-	util.PanicIf(err)
-	src, err := os.Open(srcPath)
-	util.PanicIf(err)
-	io.Copy(dest, src)
-}
-
-func (bb BucketBuilder) cleanUp() {
-	// os.RemoveAll(bb.dir)
-}
-
-func (bb BucketBuilder) javaScriptPaths() []string {
-	sourcePaths, err := filepath.Glob(filepath.Join("public/javascripts", "*"))
-	util.PanicIf(err)
-	return sourcePaths
-}
-
-func (bb BucketBuilder) stylesheetPaths() []string {
-	sourcePaths, err := filepath.Glob(filepath.Join("public/stylesheets", "*"))
-	util.PanicIf(err)
-	return sourcePaths
-}
-
-func (bb BucketBuilder) fingerprintedBaseName(path string) string {
-	data, err := ioutil.ReadFile(path)
-	util.PanicIf(err)
-	hash := md5.Sum(data)
-	nameParts := strings.SplitN(filepath.Base(path), ".", 2)
-	return fmt.Sprintf("%s-%x.%s", nameParts[0], hash, nameParts[1])
 }
